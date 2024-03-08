0c2d1e134de83d786cf229764df79b9234f4623f
Patrick <patasax22@yahoo.com>
2024-03-08 16:38:07 -0500

    reorganized spells into individual class spell list pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
-    $('.tab1').click(function(){
+    $('.tab2').click(function(){
         var attr = $(this).attr('data-li');
 
-        $('.tab1').removeClass('active1');
-        $(this).addClass('active1');
+        $('.tab2').removeClass('active2');
+        $(this).addClass('active2');
 
         $('.row').hide();
-        if(attr == 'artificer'){
+        if(attr == 'cantrip'){
             $('.' + attr).show();
         }
-        else if(attr == 'bard'){
+        else if(attr == '1st'){
             $('.' + attr).show();
         }
-        else if(attr == 'cleric'){
+        else if(attr == '2nd'){
             $('.' + attr).show();
         }
-        else if(attr == 'druid'){
+        else if(attr == '3rd'){
             $('.' + attr).show();
         }
-        else if(attr == 'paladin'){
+        else if(attr == '4th'){
             $('.' + attr).show();
         }
-        else if(attr == 'ranger'){
+        else if(attr == '5th'){
             $('.' + attr).show();
         }
-        else if(attr == 'sorcerer'){
+        else if(attr == '6th'){
             $('.' + attr).show();
         }
-        else if(attr == 'warlock'){
+        else if(attr == '7th'){
             $('.' + attr).show();
         }
-        else if(attr == 'wizard'){
+        else if(attr == '8th'){
+            $('.' + attr).show();
+        }
+        else if(attr == '9th'){
             $('.' + attr).show();
         }
         else{
@@ -40,42 +43,40 @@ $(document).ready(function() {
 });
 
 
+
 // $(document).ready(function() {
-//     $('.tab2').click(function(){
+//     $('.tab1').click(function(){
 //         var attr = $(this).attr('data-li');
 
-//         $('.tab2').removeClass('active2');
-//         $(this).addClass('active2');
+//         $('.tab1').removeClass('active1');
+//         $(this).addClass('active1');
 
 //         $('.row').hide();
-//         if(attr == 'cantrip'){
+//         if(attr == 'artificer'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '1st'){
+//         else if(attr == 'bard'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '2nd'){
+//         else if(attr == 'cleric'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '3rd'){
+//         else if(attr == 'druid'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '4th'){
+//         else if(attr == 'paladin'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '5th'){
+//         else if(attr == 'ranger'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '6th'){
+//         else if(attr == 'sorcerer'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '7th'){
+//         else if(attr == 'warlock'){
 //             $('.' + attr).show();
 //         }
-//         else if(attr == '8th'){
-//             $('.' + attr).show();
-//         }
-//         else if(attr == '9th'){
+//         else if(attr == 'wizard'){
 //             $('.' + attr).show();
 //         }
 //         else{
@@ -83,3 +84,6 @@ $(document).ready(function() {
 //         }
 //     })
 // });
+
+
+
